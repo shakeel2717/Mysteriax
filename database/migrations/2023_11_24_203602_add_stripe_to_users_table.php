0681a1355a894089b657f5d363f8b34fe3f5ee0c
Shakeel Ahmad <shakeel2717@gmail.com>
2023-11-25 01:40:36 +0500

    Migration added for Stripe

---
+++ database/migrations/2023_11_24_203602_add_stripe_to_users_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddStripeToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->after('settings', function () use ($table) {
+                $table->boolean('stripe_connect')->default(0)->after('settings');
+                $table->string('stripe_id')->nullable()->after('stripe_connect');
+                $table->string('stripe_link')->nullable()->after('stripe_id');
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
