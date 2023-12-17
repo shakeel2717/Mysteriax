d05c42ec9d740d9fc51a61b8e571dc9557a08c69
Shakeel Ahmad <shakeel2717@gmail.com>
2023-12-18 03:05:44 +0500

    modal text update

--- resources/views/livewire/vault-attach.blade.php
+++ resources/views/livewire/vault-attach.blade.php
@@ -1,7 +1,7 @@
 <div class="modal-dialog" role="document">
     <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <h5 class="modal-title" id="exampleModalLabel">Select Photo from your Vault</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
@@ -37,7 +37,6 @@
         </div>
         <div class="modal-footer">
             <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
         </div>
     </div>
 </div>
